32fa94ec06229c4cf0c8e1f6b6377022311b3912
Allannah-W <allannah.wilson05@gmail.com>
2025-09-27 13:33:28 +1000

    Refactor calculation of result

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,23 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
+
+
+def determine_result(score):
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
