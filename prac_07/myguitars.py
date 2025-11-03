408583ca207f00510b6e600472ef62344b311425
Allannah-W <allannah.wilson05@gmail.com>
2025-11-04 09:55:09 +1000

    Complete second part of More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -15,16 +15,41 @@ def load_guitars(filename):
     return guitars
 
 
+def add_new_guitar():
+    """Add new guitars via prompt and add to list"""
+    new_guitars = []
+    name = input("Name: ").strip()
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        new_guitars.append(guitar)
+        print(f"{guitar.name} added")
+        name = input("Name: ").strip()
+    return new_guitars
+
+
+def save_guitars(filename, guitars):
+    """Update list of guitars in CSV file"""
+    with open(filename, "w", encoding="utf-8") as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+
+
 def main():
     """Read guitars from CSV file and sort"""
     filename = "guitars.csv"
     guitars = load_guitars(filename)
+    new_guitars = add_new_guitar()
+    guitars.extend(new_guitars)
 
     guitars.sort()
 
     print("Guitars Sorted by Age:")
     for guitar in guitars:
         print(guitar)
+
+    save_guitars(filename, guitars)
 
 
 main()
