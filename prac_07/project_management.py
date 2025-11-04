f81384d0be4dc886a28bc481900bd78925568239
Allannah-W <allannah.wilson05@gmail.com>
2025-11-04 11:06:54 +1000

    Complete project class definition

---
+++ prac_07/project_management.py
@@ -0,0 +1,17 @@
+"""
+Project class for Project Management Program
+Estimate: 60 mins
+Actual:
+"""
+
+from datetime import datetime
+from project import Project
+
+def main():
+    """Load projects and display menu"""
+    filename = "projects.txt"
+    projects = load_projects(filename)
+
+
+
+main()
